12e747961038fcb25011ab1691f47b769e38d419
rahul-rathore786 <rahulrathorese786@gmail.com>
2025-07-12 17:15:37 +0530

    hardhat config for umi devnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,8 @@ const FORMATTED_PRIVATE_KEY = PRIVATE_KEY.startsWith("0x")
   : PRIVATE_KEY;
 const SEPOLIA_RPC_URL =
   process.env.SEPOLIA_RPC_URL || "https://sepolia.infura.io/v3/your-infura-key";
-
+const FUJI_RPC_URL =
+  process.env.FUJI_RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc";
 module.exports = {
   solidity: "0.8.18",
   networks: {
@@ -22,8 +23,26 @@ module.exports = {
       accounts: [FORMATTED_PRIVATE_KEY],
       chainId: 11155111,
     },
+    fuji: {
+      url: FUJI_RPC_URL,
+      accounts: [FORMATTED_PRIVATE_KEY],
+      chainId: 43113,
+    },
+    umi: {
+      url: "https://devnet.uminetwork.com",
+      accounts: [FORMATTED_PRIVATE_KEY],
+      chainId: 42069,
+      gasPrice: 20000000000, // 20 gwei
+      gas: 6000000, // Gas limit
+      timeout: 300000, // 5 minutes
+      blockGasLimit: 30000000,
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      allowUnlimitedContractSize: true,
+    },
   },
   paths: {
     artifacts: "./frontend/src/artifacts",
+    cache: "./frontend/src/cache",
   },
 };
